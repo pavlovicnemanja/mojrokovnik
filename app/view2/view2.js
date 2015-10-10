371000b0b6a95d5332e64c30c53511e483c2e8f4
pavlovicnemanja <me@pavlovicnemanja.com>
2015-10-10 18:47:19 +0200

    chore(client): Changed server startup from npm to grunt, and added less compiler

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -5,10 +5,10 @@ angular.module('mojrokovnik.view2', ['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/view2', {
         templateUrl: 'view2/view2.html',
-        controller: 'View2Ctrl'
+        controller: 'View2Ctrl2'
     });
 }])
 
-.controller('View2Ctrl', [function () {
+.controller('View2Ctrl2', [function () {
 
 }]);
